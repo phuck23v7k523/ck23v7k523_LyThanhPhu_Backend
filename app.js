3d0671457039b241697d33ef2372307d7df3f45b
phuck23v7k523 <phuck23v7k523@vlvh.ctu.edu.vn>
2025-03-11 00:23:01 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const contactsRouter = require("./app/routes/contact.route");
 const cors = require('cors');
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -12,4 +13,16 @@ app.get('/', (req, res) => {
 });
 
 app.use("/api/contacts", contactsRouter)
+// handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
+
+
 module.exports = app;
